689f750e77e96658479d47a321d77a2b22b086a8
hamza4600 <78164997+hamza4600@users.noreply.github.com>
2022-02-19 10:05:29 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Basic from './components/basic'
+import Flexbox from './components/flexbox'
+import Header from './components/header'
+
+function index() {
+  return (
+    <div>
+        
+        <Header/>
+        <Flexbox/>
+
+        <div className='flex align-center m-10 p-3  '>
+ 
+        <p>hamz a Khan</p>
+        </div>
+ 
+    </div>
+  )
+}
+
+export default index
